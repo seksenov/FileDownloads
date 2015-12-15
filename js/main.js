bf18dd4a10a7f44d907fdf794df337366de3cc1a
Kiril Seksenov <seksenovk@gmail.com>
2015-12-15 14:11:00 -0800

    Update 2015-12-15T22:10:52.973Z

--- js/main.js
+++ js/main.js
@@ -1,85 +1,99 @@
-'use strict'
+;(function() {
+  'use strict'
 
-var client = null;
-
-document.addEventListener("DOMContentLoaded", init);
-
-function init() {
-	var btn = document.getElementById("saveFile");
-	btn.addEventListener("click", function() {downloadAndSave("./img/detroitSkyline.jpg")});
-}
-
-function downloadAndSave(url) {
-	if (typeof Windows !== 'undefined') {
-		client = new XMLHttpRequest();
-    	if (client) {
-	        client.open("GET", url, true);
-	        client.responseType = "blob";
-	        client.onreadystatechange = readyStateCallback;
-	        client.send(null);
-	    } else {
-	       console.log("Cannot create new XMLHttpRequest object");
-	    }
-	} else {
-		console.log("Windows namespace is undefined. Make sure this is running as a hosted web app with WinRT access through ACURs/Content URIs")
+	function download(url) {
+		return new Promise(function(resolve, reject) {
+			if (typeof Windows === 'undefined') {
+				reject(new Error("Windows namespace is undefined. Make sure this is running as a hosted web app with WinRT access through ACURs/Content URIs"));
+				return;
+			}
+			var client = new XMLHttpRequest();
+			client.open("GET", url, true);
+			client.responseType = "blob";
+			client.onreadystatechange = function() {
+				if (this.readyState === 4) {
+					if (this.status === 200) {
+						resolve(this.response);
+					} else {
+						this.onerror();
+					}
+				}
+			};
+			client.onerror = function() {
+				reject(new Error("Unable to download blob - status code: " + this.status));
+			};
+			client.send(null);
+		});
 	}
 
-	
-}
+	function saveBlob(blob) {
+		// Get the IInputStream stream from the blob object
+		var inputStream = blob.msDetachStream();
 
-function readyStateCallback() {
-    if (client.readyState === 4) {
-        if (client.status !== 200) {
-            console.log("Unable to download blob - status code: " + client.status.toString());
-        } else {
-            var blob = client.response;
-            saveFile(blob);
-        }
-    }
-}
+		var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+		savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+		savePicker.fileTypeChoices.insert("JPEG (*.jpg)", [".jpg"]);
+		savePicker.suggestedFileName = "NewPicture";
 
-function saveFile(blob) {
-	// Clean scenario output
-    console.log("saveFile Invoked");
+		var theStorageFile = null;
 
-     // Create the picker object and set options
-    var savePicker = new Windows.Storage.Pickers.FileSavePicker();
-    savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
-    // Dropdown of file types the user can save the file as
-    savePicker.fileTypeChoices.insert("JPEG (*.jpg)", [".jpg"]);
-    // Default file name if the user does not type one in or select a file to replace
-    savePicker.suggestedFileName = "NewPicture";
+		return new Promise(function(resolve, reject) {
+			savePicker
+				.pickSaveFileAsync()
+				.then(deferUpdates)
+				.then(function(storageFile) {
+					theStorageFile = storageFile;
+					return new Promise(function(resolve) {
+						// Open the returned file in order to copy the data
+						storageFile.openAsync(Windows.Storage.FileAccessMode.readWrite).then(function(outputStream) {
+							// Copy the stream from the blob to the File stream
+							Windows.Storage.Streams.RandomAccessStream.copyAsync(inputStream, outputStream).then(function() {
+								outputStream.flushAsync().done(function() {
+									inputStream.close();
+									outputStream.close();
+									resolve(storageFile);
+								});
+							});
+						});
 
-    savePicker.pickSaveFileAsync().then(function (file) {
-        if (file) {
-        	// Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
-            Windows.Storage.CachedFileManager.deferUpdates(file);
-        	// Open the returned file in order to copy the data
-        	file.openAsync(Windows.Storage.FileAccessMode.readWrite).then(function (output) {
-	            // Get the IInputStream stream from the blob object
-	            var input = blob.msDetachStream();
+						return storageFile;
+					});
+				})
+				.then(completeUpdates)
+				.then(function(status) {
+					if (status === Windows.Storage.Provider.FileUpdateStatus.complete) {
+						resolve(theStorageFile);
+					} else {
+						reject(theStorageFile);
+					}
+				});
+		});
+	}
 
-	            // Copy the stream from the blob to the File stream
-	            Windows.Storage.Streams.RandomAccessStream.copyAsync(input, output).then(function () {
-	                output.flushAsync().done(function () {
-	                    input.close();
-	                    output.close();
-	                    console.log("File: " + file.name + " saved successfully to the Pictures Library!");
+	function deferUpdates(storageFile) {
+		// Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
+		Windows.Storage.CachedFileManager.deferUpdates(storageFile);
+		return Promise.resolve(storageFile);
+	}
 
-	                    // Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
-		                // Completing updates may require Windows to ask for user input.
-		                Windows.Storage.CachedFileManager.completeUpdatesAsync(file).done(function (updateStatus) {
-		                    if (updateStatus === Windows.Storage.Provider.FileUpdateStatus.complete) {
-		                       console.log("File " + file.name + " was saved.");
-		                    } else {
-		                       console.log("File " + file.name + " couldn't be saved.");
-		                    }
-		                });
-	                });
-	            });
-        	});
-        } else {
-            console.log("Operation cancelled.");
-        }
-    });
-}
+	function completeUpdates(storageFile) {
+		return new Promise(function(resolve) {
+			Windows.Storage.CachedFileManager.completeUpdatesAsync(storageFile).done(resolve);
+		});
+	}
+
+	document.addEventListener("DOMContentLoaded", function() {
+		document.getElementById("saveFile")
+		  .addEventListener("click", function () {
+				download("./img/detroitSkyline.jpg")
+				  .then(saveBlob)
+					.then(function(file) {
+						console.log("File " + file.name + " was saved.");
+					})
+					.catch(function(file) {
+						console.log("File " + file.name + " couldn't be saved.");
+					});
+			});
+	});
+
+}());
